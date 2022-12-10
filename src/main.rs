123b76217bef6ca7af2b439c4bb6c3ead8eb2da8
phantie <funtom1605@gmail.com>
2022-12-10 18:22:03 +0200

    Proximity to objects system

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-#![allow(dead_code, unused_imports)]
-use std::borrow::BorrowMut;
-use std::f32::consts::SQRT_2;
-
 // TODO fix pause screen might not be in front of the whole scene
 
+#![allow(dead_code, unused_imports)]
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
+use bevy::utils::HashMap;
+use std::borrow::BorrowMut;
+use std::f32::consts::SQRT_2;
 
 mod unused_systems;
 use crate::unused_systems::*;
@@ -30,6 +30,13 @@ fn main() {
         .insert_resource(ClearColor(Color::DARK_GRAY))
         .add_startup_system(set_up_camera)
         .add_startup_system(spawn_player)
+        .add_startup_system(spawn_npcs)
+        .insert_resource(ProximityToNPCsResource::default())
+        .add_system(next_to_obj_watcher)
+        .add_event::<NextToObjEvent>()
+        .add_event::<AwayFromObjEvent>()
+        .add_system(next_to_npc_event_handler)
+        .add_system(away_from_npc_event_handler)
         .add_state(AppState::InGame)
         // move player only when InGame
         .add_system_set(SystemSet::on_update(AppState::InGame).with_system(player_control))
@@ -48,7 +55,7 @@ fn set_up_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-#[derive(Debug, Component)]
+#[derive(Component)]
 struct Player;
 
 #[derive(Debug, Component)]
@@ -84,6 +91,28 @@ struct PlayerBundle {
     _p: Player,
 }
 
+#[derive(Component)]
+struct NPC;
+
+#[derive(Component)]
+struct InProximity {
+    edge_distance: f32,
+}
+
+// #[derive(Component)]
+// struct TalkTo;
+
+// #[derive(Component)]
+// struct Interact;
+
+#[derive(Bundle)]
+struct NPCBundle {
+    name: Name,
+    model: SpriteBundle,
+    _n: NPC,
+    in_proximity: InProximity,
+}
+
 fn spawn_player(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -101,6 +130,155 @@ fn spawn_player(
     });
 
     info!("Spawning a player");
+}
+
+fn spawn_npcs(mut commands: Commands) {
+    commands.spawn(NPCBundle {
+        name: "Joe".into(),
+        in_proximity: InProximity {
+            edge_distance: 150.,
+        },
+        _n: NPC,
+        model: SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(100., 100.)),
+                ..default()
+            },
+            transform: Transform::from_xyz(200., 0., 0.),
+            ..default()
+        },
+    });
+
+    commands.spawn(NPCBundle {
+        name: "Rue".into(),
+        in_proximity: InProximity {
+            edge_distance: 150.,
+        },
+        _n: NPC,
+        model: SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(100., 100.)),
+                ..default()
+            },
+            transform: Transform::from_xyz(-200., 100., 0.),
+            ..default()
+        },
+    });
+
+    commands.spawn(NPCBundle {
+        name: "Moe".into(),
+        in_proximity: InProximity {
+            edge_distance: 150.,
+        },
+        _n: NPC,
+        model: SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(100., 100.)),
+                ..default()
+            },
+            transform: Transform::from_xyz(-350., 100., 0.),
+            ..default()
+        },
+    });
+
+    info!("Spawning NPC");
+}
+
+#[derive(Resource)]
+struct ProximityToNPCsResource {
+    values: HashMap<Entity, bool>,
+}
+
+impl Default for ProximityToNPCsResource {
+    fn default() -> Self {
+        Self {
+            values: HashMap::new(),
+        }
+    }
+}
+
+struct NextToObjEvent {
+    entity: Entity,
+}
+struct AwayFromObjEvent {
+    entity: Entity,
+}
+
+fn next_to_obj_watcher(
+    rel_obj_transforms: Query<(Entity, &Transform, &InProximity)>,
+    player_transform: Query<&Transform, With<Player>>,
+    mut next_to_obj: ResMut<ProximityToNPCsResource>,
+    mut ev_next_to_obj: EventWriter<NextToObjEvent>,
+    mut ev_away_from_obj: EventWriter<AwayFromObjEvent>,
+) {
+    let player_transform = player_transform.single();
+
+    fn is_in_proximity(
+        player_transform: &Transform,
+        rel_obj_transform: &Transform,
+        in_proximity: &InProximity,
+    ) -> bool {
+        let next_to_when_distance_is_less_than = in_proximity.edge_distance;
+
+        rel_obj_transform
+            .translation
+            .distance(player_transform.translation)
+            < next_to_when_distance_is_less_than
+    }
+
+    for (entity, obj_transform, in_proximity) in &rel_obj_transforms {
+        let next_to = next_to_obj.values.get(&entity);
+
+        if is_in_proximity(&player_transform, obj_transform, in_proximity) {
+            match next_to {
+                None | Some(false) => {
+                    next_to_obj.values.insert(entity, true);
+                    ev_next_to_obj.send(NextToObjEvent { entity });
+                }
+                Some(true) => (),
+            }
+        } else {
+            match next_to {
+                Some(true) => {
+                    next_to_obj.values.insert(entity, false);
+                    ev_away_from_obj.send(AwayFromObjEvent { entity });
+                }
+                Some(false) => (),
+                // if you never've been close don't send AwayFrom event
+                // not sure if the same should apply to NextTo event
+                None => (),
+            }
+        }
+    }
+}
+
+fn next_to_npc_event_handler(
+    mut ev_next_to_obj: EventReader<NextToObjEvent>,
+    query: Query<(Entity, &Name), With<NPC>>,
+) {
+    for ev in ev_next_to_obj.iter() {
+        for (entity, name) in &query {
+            if entity == ev.entity {
+                info!("Next to NPC {}", name.value);
+            }
+        }
+    }
+}
+
+fn away_from_npc_event_handler(
+    mut ev_away_from_obj: EventReader<AwayFromObjEvent>,
+    query: Query<(Entity, &Name), With<NPC>>,
+) {
+    for ev in ev_away_from_obj.iter() {
+        for (entity, name) in &query {
+            if entity == ev.entity {
+                info!("Away from NPC {}", name.value);
+            }
+        }
+    }
 }
 
 // fn debug_player(
