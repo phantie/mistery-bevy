08b649e8826520c786484853fcfd0b5f237d6d56
phantie <funtom1605@gmail.com>
2023-01-01 04:27:25 +0200

    Final commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// It was educational, sorta fun. But I the effort you have to put
+// to make all work as you intend not worth the inverstment.
+
+// The framework is too raw.
+// Main issues:
+//      - no good way to scale game for different view ports
+//          - what I could find already does not work in this
+//            engine version, what I can think of seems awkward
+//            if accomplishable at all
+//      - no UI for level design
+//      - no UI for UI design
+//      - the issue #1 is the main one
+//
+// I had greater hopes, but failing fast is a good thing nonetheless
+
+
+
 // NOTE current implementation of NPC proximity does not take into
 //     account lengths when several objects are considered to be in proximity
 //     now, the "closest object" is the latest object detected to be in proximity
